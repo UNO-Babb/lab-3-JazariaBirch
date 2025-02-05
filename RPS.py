db54311325ac50e55a832bfd588cd1803d44b996
JazariaBirch <JazariaBirch@unomaha.edu>
2025-02-05 16:16:25 +0000

    Done With Lab

--- RPS.py
+++ RPS.py
@@ -10,11 +10,49 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  
+  play = "Y"
+  while play == "Y":
 
   #Randomly choose the computer between 'R', 'P', or 'S'
   #Prompt the user for their RPS selection
   #Determine winner and state what happened to the user
   #Ask the user if they would like to play again.
+
+    player = input("Choice (Rock,Paper,Scissors):")
+    
+    computor = random.choice (["Rock" , "Paper" , "Scissors"])
+    if computor == "Rock":
+      print("Opponent chose Rock:")
+    if computor == "Paper":
+      print("Opponent chose Paper")
+    if computor == "Scissors":
+      print("Opponent chose scissors")
+
+    if player == computor :
+      print("Tie")
+      ties = ties + 1
+    elif player == "Rock" and computor == "Scissors":
+      print("You Win!:D")
+      wins = wins + 1
+    elif player == "Rock" and computor == "Paper":
+      print("You Lose.D:")
+      losses = losses + 1
+    elif player == "Paper" and computor == "Rock":
+      print("You Win!:D")
+      wins = wins + 1
+    elif player == "Paper" and computor == "Scissors":
+      print("You Lose.D:")
+      losses - losses + 1
+    elif player == "Scissors" and computor == "Rock":
+      print("You Lose.D:")
+      losses = losses + 1
+    elif player == "Scissors" and computor == "Paper":
+      print("You Win!:D")
+      wins = wins + 1
+
+      
+    play = input("play again? (y/n): ")
 
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
--- TempConvert.py
+++ TempConvert.py
@@ -8,10 +8,14 @@ def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
+  tempF = input("Enter a Fahrenheit temperture:")
+  tempF = int(tempF)
+  tempC = (tempF - 32 ) * 5 / 9
 
-  tempC = tempF / 2
+  num = tempC
+  num = round(num, 2)
+  print(num)
 
-  print(tempF, "is ", tempC, "degrees celsius.")
+  print(tempF, "is ", num, "degrees celsius.")
 if __name__ == '__main__':
   main()
